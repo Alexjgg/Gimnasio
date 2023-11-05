1d418e2bfac9e8e579421051906ed6e7a877d0f8
alexjgg <alexgiraldezgroba@gmail.com>
2023-11-05 18:12:07 +0100

    Comentarios en el config para aclarar

--- config.php
+++ config.php
@@ -11,12 +11,17 @@ $config->set('DATA_FOLDER', $config->get('APP_FOLDER') . 'Data/');
 $config->set('DEFAULT_CONTROLLER', 'Inicio');
 $config->set('DEFAULT_ACTION', 'index');
 
-
+//Si estamos en producion esto se cambia a false
 $config->set('DEBUG', true);
 
+//La direcion de nuestra base de datos cambiar si tu servidor no esta en localhost
 $config->set('dbhost', 'localhost');
+//El schema de la base de datos que vamos a usar si importamos el archivo dado no hace falta cambiar
 $config->set('dbname', 'gimnasiobd');
+//El nombre de usuario que tiene un admin el la base de datos o al menos que tenga permisos para ver y modificar gimansiobd
 $config->set('dbuser', 'root');
+//La contraseña de el usuario
 $config->set('dbpass', '');
 $config->set('dbcharset', 'utf8mb4');
+
 $config->set('emulado', false);
